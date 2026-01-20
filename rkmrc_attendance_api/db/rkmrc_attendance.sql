b1d61f492c437a198619fd63858fd47f5ec20565
avirup04 <avirupmukherjee019@gmail.com>
2026-01-20 09:29:15 +0530

    updated db
    updated the db in xampp

--- rkmrc_attendance_api/db/rkmrc_attendance.sql
+++ rkmrc_attendance_api/db/rkmrc_attendance.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 19, 2026 at 05:47 PM
+-- Generation Time: Jan 20, 2026 at 04:58 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.1.25
 
@@ -45,7 +45,9 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `roll_no`, `dept`, `semester`, `email`, `mobile`, `password`, `is_setup_done`) VALUES
 (1, 'LSUG/124/25', 'Physics', 1, 'ahgagahg@gmail.com', NULL, '$2y$10$80QKpKxSMeAbr63tbEwnjOh8tyqpqG//GyiAjq91e1/zu93t9R9wq', 0),
 (2, 'LSUG/124/29', 'Life Science', 2, 'fkhdhh@gmail.com', '9609173472', '$2y$10$uyvO5Cx9d7W5BqlYtPI7DekOsxOSA2bb6NHcgaKEnmCRQ1myFz6su', 0),
-(58, 'LSUG12425', 'Life Science', 2, 'avirupmukherjee019@gmail.com', '9609173472', '$2y$10$SgncZaa4J4KxAeaDxPAiXun0yZDn09O970Vy1ZRCpWCghjQGZq9OG', 0);
+(58, 'LSUG12425', 'Life Science', 2, 'avirupmukherjee019@gmail.com', '9609173472', '$2y$10$SgncZaa4J4KxAeaDxPAiXun0yZDn09O970Vy1ZRCpWCghjQGZq9OG', 0),
+(61, 'LSUG12429', 'Physics', 2, 'phy@gmail.com', '8796542365', '$2y$10$Os8GAFMNVebDeW2vkQu47Okxv8GIUrXFAQ3aFMK3YO1wzj/wtOEs6', 0),
+(62, 'PHUG12425', 'Physics', 2, 'phy@gmail.com', '1234567890', '$2y$10$q6jxK81.KHdD8sOj9f20U.kicpgwH3aPDV5oPTS3C1ombIBjJiGDi', 0);
 
 --
 -- Indexes for dumped tables
@@ -66,7 +68,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
