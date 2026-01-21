bae73bcc7eab3328f4be361781331070b48b58db
Avik1233781 <tanusreemakal6@gmail.com>
2026-01-21 11:22:23 +0530

    Updated db structure

--- rkmrc_attendance_api/db/rkmrc_attendance.sql
+++ rkmrc_attendance_api/db/rkmrc_attendance.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 20, 2026 at 09:15 AM
+-- Generation Time: Jan 21, 2026 at 06:51 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -30,6 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `roll_no` varchar(50) NOT NULL,
+  `name` varchar(100) NOT NULL,
   `dept` varchar(100) NOT NULL,
   `semester` int(11) NOT NULL,
   `email` varchar(100) NOT NULL,
@@ -42,14 +43,13 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `roll_no`, `dept`, `semester`, `email`, `mobile`, `password`, `is_setup_done`) VALUES
-(1, 'LSUG/124/25', 'Physics', 1, 'ahgagahg@gmail.com', NULL, '$2y$10$80QKpKxSMeAbr63tbEwnjOh8tyqpqG//GyiAjq91e1/zu93t9R9wq', 0),
-(2, 'LSUG/124/29', 'Life Science', 2, 'fkhdhh@gmail.com', '9609173472', '$2y$10$uyvO5Cx9d7W5BqlYtPI7DekOsxOSA2bb6NHcgaKEnmCRQ1myFz6su', 0),
-(58, 'LSUG12425', 'Life Science', 2, 'avirupmukherjee019@gmail.com', '9609173472', '$2y$10$SgncZaa4J4KxAeaDxPAiXun0yZDn09O970Vy1ZRCpWCghjQGZq9OG', 0),
-(61, 'LSUG12429', 'Physics', 2, 'phy@gmail.com', '8796542365', '$2y$10$Os8GAFMNVebDeW2vkQu47Okxv8GIUrXFAQ3aFMK3YO1wzj/wtOEs6', 0),
-(62, 'PHUG12425', 'Physics', 2, 'phy@gmail.com', '1234567890', '$2y$10$q6jxK81.KHdD8sOj9f20U.kicpgwH3aPDV5oPTS3C1ombIBjJiGDi', 0),
-(63, 'LSUG26625', 'Life_Sciences', 2, 'biswastaniska06@gmail.com', '8250256224', '$2y$10$Yy3olYMK7O0dvr3CWkSYEeNdG1hwNjkMiX3cWqvBemyGxDo9we/oW', 0),
-(64, 'LSUG04625', 'Life_Sciences', 2, 'rupayansingha2@gmail.com', '9143480258', '$2y$10$Ttf2bLk3D6zC3MCHFZNeLOYXX388tePrXnnGkHf6b5vsJ8oHGQjmq', 0);
+INSERT INTO `users` (`id`, `roll_no`, `name`, `dept`, `semester`, `email`, `mobile`, `password`, `is_setup_done`) VALUES
+(1, 'LSUG/124/25', '', 'Physics', 1, 'ahgagahg@gmail.com', NULL, '$2y$10$80QKpKxSMeAbr63tbEwnjOh8tyqpqG//GyiAjq91e1/zu93t9R9wq', 0),
+(2, 'LSUG/124/29', '', 'Life Science', 2, 'fkhdhh@gmail.com', '9609173472', '$2y$10$uyvO5Cx9d7W5BqlYtPI7DekOsxOSA2bb6NHcgaKEnmCRQ1myFz6su', 0),
+(61, 'LSUG12429', '', 'Physics', 2, 'phy@gmail.com', '8796542365', '$2y$10$Os8GAFMNVebDeW2vkQu47Okxv8GIUrXFAQ3aFMK3YO1wzj/wtOEs6', 0),
+(62, 'PHUG12425', '', 'Physics', 2, 'phy@gmail.com', '1234567890', '$2y$10$q6jxK81.KHdD8sOj9f20U.kicpgwH3aPDV5oPTS3C1ombIBjJiGDi', 0),
+(64, 'LSUG04625', '', 'Life_Sciences', 2, 'rupayansingha2@gmail.com', '9143480258', '$2y$10$Ttf2bLk3D6zC3MCHFZNeLOYXX388tePrXnnGkHf6b5vsJ8oHGQjmq', 0),
+(66, 'LSUG12425', 'Avirup Mukherjee', 'Life_Sciences', 2, 'avirupmukherjee019@gmail.com', '9609173472', '$2y$10$Cg0yMyWXiAa695kEzazjdO0U8gm3LAuwwuO4.9zIlQ/gT379HJ.ky', 0);
 
 --
 -- Indexes for dumped tables
@@ -70,7 +70,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=67;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
